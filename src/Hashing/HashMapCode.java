86eb7888316ec2b75b4b8c329fa0ce68cd5885e3
jasmeet2335 <jasmeetkaur9047@gmail.com>
2024-08-05 23:47:51 -0400

    HashMapCode

---
+++ src/Hashing/HashMapCode.java
@@ -0,0 +1,22 @@
+package Hashing;
+import java.util.*;
+
+public class HashMapCode {
+    static class HashMap<K,V> {  //generic
+        private class Node  { }
+            private int size;
+            private LinkedList <Node> buckets[];
+
+            @SuppressWarnings("unchecked")
+            public HashMap() {}
+
+            public void put(K key, V value) {
+                
+            }
+
+         }
+    
+    public static void main(String args[] ){
+
+    }
+}
