3b8db167c6101cc8fd4b568c93dce4047775dba5
jasmeet2335 <jasmeetkaur9047@gmail.com>
2024-08-07 23:31:18 -0400

    trie files added

---
+++ src/Trie/prefixProblem.java
@@ -0,0 +1,65 @@
+package Trie;
+
+public class prefixProblem {
+    /*
+     * Find shortest unique prefix for every word in  
+     * given list. Assume no word is prefix of another
+     * arr[] = {"zebra", "dog", "duck", "dove"}
+     * ans = {"z", "dog", "du", "dov"}
+     * 
+     */
+    static class Node{
+        Node[] children = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        public Node(){
+            for(int i = 0 ; i <children.length; i ++){
+                children[i] = null;
+            }
+            freq =1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for(int i = 0; i <word.length(); i ++){
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static void findPrefix(Node root , String ans){ //O(L) Longest word in trie
+        if(root == null){
+
+        }
+        if(root.freq == 1){
+            System.out.println(ans);
+            return;
+        }
+        for(int i =0 ; i<root.children.length ; i ++){
+            if(root.children[i] != null){
+                findPrefix(root.children[i], ans+ (char)(i+'a'));
+            }
+        }
+    }
+    public static void main(String args[]){
+        String arr[] = {"zebra", "dog", "duck", "dove"};
+
+        for(int i = 0 ; i <arr.length ; i ++){
+            insert(arr[i]);
+        }
+
+        root.freq = -1;
+
+        findPrefix(root, "");
+    }
+}
